b2cafe07aa57e9e5ada75ebb5369740841cedaa2
Bartek Rutkowski <bartekrutkowski@users.noreply.github.com>
2015-03-23 13:10:19 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,6 +50,7 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
+  include git
   include iterm2::stable
   include iterm2::colors::solarized_dark
   include xtrafinder
