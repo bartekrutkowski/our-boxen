bea628a644b6836d98c838af701d69fbe9ae3484
Bartek Rutkowski <bartekrutkowski@users.noreply.github.com>
2015-03-23 13:08:37 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -60,6 +60,10 @@ node default {
     version => '36.0.4'
   }
 
+  appstore::app { 'Wunderlist':
+    source => 'wunderlist-to-do-list-tasks/id410628904', # required
+  }
+
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
